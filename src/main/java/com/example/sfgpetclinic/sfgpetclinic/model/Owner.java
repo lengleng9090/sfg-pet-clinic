15d27f2706c6cfe7248f23e2364321a93cf17208
UntillNIGHT <lengleng9090@gmail.com>
2023-01-08 22:51:01 +0700

    [ADD] POJOs model

---
+++ src/main/java/com/example/sfgpetclinic/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.example.sfgpetclinic.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/example/sfgpetclinic/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.example.sfgpetclinic.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
